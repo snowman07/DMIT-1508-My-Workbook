b71b700cc4080d9bd64324ecc685c725feb9db31
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-02-07 13:48:57 -0700

    check constraints, indexes, alter tables

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -36,11 +36,24 @@ CREATE TABLE Students
 		IDENTITY(20200001, 1)		NOT NULL,
 		
 	GivenName		varchar(50)		NOT NULL,
-	Surname			varchar(50)		NOT NULL,
+
+---	 % is a wildcard for zero or more characters (letter, digit, or other character)
+---	_ is a wildcard for a single character (letter, digit, or other character)
+---	[] are used to represent a range or set of characters that are allowed
+	Surname			varchar(50)		
+		CONSTRAINT CK_Students_Surname
+--			CHECK (Surname LIKE '_%')	--- LIKE allows us to do a "pattern-match"
+			CHECK (Surname LIKE '[a-z][a-z]')	-- two letter plus any other chars
+--								\  1/ \ 1   /
+		--	Positive match for 'Fred'
+		--	Positive match for 'Wu'
+		-- Negative match for 'F'
+		-- Negative match for '2udor'
+									NOT NULL,
 	DateOfBirth		datetime		NOT NULL,
 	Enrolled		bit				
 		CONSTRAINT DF_Students_Enrolled
-			DEFAULT (1)				NOT NULL
+			DEFAULT (1)				NOT NULL --- default value if there's no value supplied
 )
 
 CREATE TABLE Courses
@@ -49,12 +62,25 @@ CREATE TABLE Courses
 		CONSTRAINT PK_Courses_Number
 			PRIMARY KEY				NOT NULL,
 	[Name]			varchar(50)		NOT NULL,
-	Credits			decimal(3,1)	NOT NULL,
-	[Hours]			tinyint			NOT NULL,
+	Credits			decimal(3,1)	
+		CONSTRAINT CK_Courses_Credits
+			CHECK (Credits > 0 AND Credits <= 12) --- This is a check constraints	
+			---		\		/		\			/
+			---		 boolean		   boolean
+			---		  \						 /
+			---				 boolean
+									NOT NULL,
+	[Hours]			tinyint			
+		CONSTRAINT CK_Courses_Hours
+---			CHECK ([Hours] >= 15 AND [Hours] <= 180)
+			CHECK ([Hours] BETWEEN 15 AND 180) ---BETWEEN operator is inclusive
+									NOT NULL,
 	Active			bit				
 		CONSTRAINT DF_Courses_Active
-			DEFAULT (1)				NOT NULL,
-	Cost			money			NOT NULL
+			DEFAULT (1)				NOT NULL,	--- this is a default constraint
+	Cost			money			
+		CONSTRAINT CK_Courses_Cost
+			CHECK (Cost >= 0)		NOT NULL
 )
 
 CREATE TABLE StudentCourses
@@ -70,8 +96,54 @@ CREATE TABLE StudentCourses
 	[Year]			tinyint			NOT NULL,
 	Term			char(3)			NOT NULL,
 	FinalMark		tinyint			NULL,
-	[Status]		char(1)			NOT NULL
+	[Status]		char(1)			
+		CONSTRAINT CK_StudentCourses_Status
+			CHECK ( [Status] = 'E' OR
+					[Status] = 'C' OR
+					[Status] = 'W')
+		--	CHECK ([Status] IN ('E', 'C', 'W'))
+		CONSTRAINT DF_StudetnCourses_Status
+			DEFAULT ('E')
+									NOT NULL,
+
 	-- Table-level definition for Composite Primary Keys
 	CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
-		PRIMARY KEY (StudentID, CourseNumber)
+		PRIMARY KEY (StudentID, CourseNumber),
+	-- Table-level constraint involving more than one column
+	CONSTRAINT CK_StudentCourses_FinalMark_Status
+		CHECK (([Status] = 'C' AND FinalMark IS NOT NULL)
+				OR
+				([Status] IN ('E', 'W') AND FinalMark IS NULL))
 )
+
+
+/*-----INDEXES-----*/
+-- For all foreign keys
+CREATE NONCLUSTERED INDEX IX_StudentCourses_StudentID
+	ON StudentCourses (StudentID)
+
+CREATE NONCLUSTERED INDEX IX_StudentCourses_CourseNumber
+	ON StudentCourses (CourseNumber)
+
+
+-- For other columns where searching/sorting might be important
+CREATE NONCLUSTERED INDEX IX_Student_Surname
+	ON Students (Surname)
+GO --- one batch
+
+
+/*-----ALTER TABLE STATEMENTS-----*/
+-- 1) Add a PostalCode for the Student Table
+ALTER TABLE Students
+	ADD PostalCode char(6) NULL
+	-- Adding this as a nullable column, because students already exists,
+	-- and we don'thave postal code for those students.
+GO	-- I have to break the above code as a separate batch from the following 
+	-- second batch
+
+-- 2) Make sure the PostalCode follows the correct pattern A#A#A#
+ALTER TABLE Students
+	ADD CONSTRAINT CK_Students_PostalCode
+		CHECK (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]')
+		-- Match for T4R1H2:	  T    4    R    1    H    2
+GO	-- third batch
